217fb2b701f47f8bbc88dd72314f47124dc6bbc5
Gary Gao <garygaowork@gmail.com>
2015-09-11 14:37:52 +0800

    Create database.py

---
+++ python/using_sqlalchemy01/database.py
@@ -0,0 +1,15 @@
+from .models import Session
+from contextlib import contextmanager
+
+@contextmanager
+def session_scope():
+    """Provide a transactional scope around a series of operations."""
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
