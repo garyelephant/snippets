21ec3b02668e4622679529891be684d1df4dd56c
Gary Gao <garygaowork@gmail.com>
2018-04-15 18:01:29 +0800

    Update spark-shell-example.scala

--- spark/spark-shell-example.scala
+++ spark/spark-shell-example.scala
@@ -1,5 +1,9 @@
-
+// Example 1:
 val df = spark.read.json("../examples/src/main/resources/people.json")
 df.createOrReplaceTempView("people")
 val sqlDF = spark.sql("SELECT * FROM people")
 sqlDF.show()
+
+
+// Example 2:
+val df1 = Seq("a").toDF
